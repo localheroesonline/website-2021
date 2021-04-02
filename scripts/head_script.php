8d0f23419b3942182b2ce4fdb388d4f165c05621
Mickpunt <perez.mick@gmail.com>
2021-04-02 15:33:06 +0200

    Website as delivered by Varun from Actiknow

---
+++ scripts/head_script.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Local Heroes | Home</title>
+<link rel="stylesheet" href="resources/bootstrap/css/bootstrap.min.css">
+<!-- GOOGLE FONTS -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap" rel="stylesheet">
+
+<!-- FONT AWESOME 5 -->
+<link rel="stylesheet" href="resources/fontawesome/all.css" />
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick-theme.min.css">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.8/slick.css">
+
+<link rel="stylesheet" href="css/main.css">
