032a888d4ee9d7ccad4757373a9779bda040ab92
Dirk Engels <d.engels@dirkengels.com>
2021-04-14 12:26:04 +0200

    fallback to iOS store and some extra logging

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -144,18 +144,25 @@ function getMobileOperatingSystem() {
 function DetectAndServe(campaign) {
   let os = getMobileOperatingSystem();
   // window.alert(os);
+  console.log('Campaign: ' + campaign);
+  console.log('OS: ' + os);
   if (os == "Android") {
       mixpanel.track("Goto App Marketplace", {
           "Marketplace": "Android",
           "Campaign": campaign,
       });
+      console.log('Redirect to Android store');
       window.location.href = "https://play.google.com/store/apps/details?id=com.localheroes.consumer";
   } else if (os == "iOS") {
       mixpanel.track("Goto App Marketplace", {
         "Marketplace": "iOS",
         "Campaign": campaign,
       });
+      console.log('Redirect to iOS store');
       window.location.href = "https://apps.apple.com/us/app/lh-consumer/id1470938037";
+  /*
+  // @todo: I disabled the redirect to the homepage as it is useless.
+  // Redirect to apple store in case it is not android. Fix later.
   } else if (os == "Windows Phone") {
       mixpanel.track("Goto Local Heroes Home", {
           "Campaign": campaign,
@@ -166,7 +173,16 @@ function DetectAndServe(campaign) {
         "Campaign": campaign,
       });
       window.location.href = "/";
+  */
   }
-  window.alert(campaign);
+
+  console.log('Fallback to iOS store');
+  mixpanel.track("Goto iOS Marketplace (unknown)", {
+    "Marketplace": "Unknown",
+    "Campaign": campaign,
+  });
+  window.location.href = "https://apps.apple.com/us/app/lh-consumer/id1470938037";
+//  window.alert(campaign);
+
   //window.alert(window.location.href);
 };
